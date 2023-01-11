3719ae63a0852036d808fdd2b5eead6e80576289
Michel Brito <michel.brito.9699@gmail.com>
2023-01-11 01:02:39 -0300

    implementado get de lista de produtos e cors na security configuration

--- src/main/java/br/com/precopoint/PrecoPoint/controller/ProdutoController.java
+++ src/main/java/br/com/precopoint/PrecoPoint/controller/ProdutoController.java
@@ -3,17 +3,16 @@ package br.com.precopoint.PrecoPoint.controller;
 
 import br.com.precopoint.PrecoPoint.dto.produto.ProdutoRequestDto;
 import br.com.precopoint.PrecoPoint.dto.usuario.StatusResponseDto;
+import br.com.precopoint.PrecoPoint.model.Produto;
 import br.com.precopoint.PrecoPoint.repository.FornecedorRepository;
 import br.com.precopoint.PrecoPoint.repository.ProdutoRepository;
 import br.com.precopoint.PrecoPoint.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/produto")
@@ -31,5 +30,10 @@ public class ProdutoController {
     @PostMapping("addproduto")
     ResponseEntity<StatusResponseDto> addProduto(@Valid @RequestBody ProdutoRequestDto request) throws Exception {
         return produtoService.addProduto(request);
+    }
+
+    @GetMapping("list-produto")
+    ResponseEntity<List<Produto>> getProduto(){
+        return produtoService.getProduto();
     }
 }
--- src/main/java/br/com/precopoint/PrecoPoint/service/ProdutoService.java
+++ src/main/java/br/com/precopoint/PrecoPoint/service/ProdutoService.java
@@ -2,12 +2,15 @@ package br.com.precopoint.PrecoPoint.service;
 
 import br.com.precopoint.PrecoPoint.dto.produto.ProdutoRequestDto;
 import br.com.precopoint.PrecoPoint.dto.usuario.StatusResponseDto;
+import br.com.precopoint.PrecoPoint.model.Produto;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.validation.Valid;
+import java.util.List;
 
 public interface ProdutoService {
 
     ResponseEntity<StatusResponseDto> addProduto(ProdutoRequestDto request) throws Exception;
+    ResponseEntity<List<Produto>> getProduto();
 }
